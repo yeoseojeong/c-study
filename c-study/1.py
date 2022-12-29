0d070aa6b41afc444c18dd6dad2190cf6a47e936
yeoseojeong <121150215+yeoseojeong@users.noreply.github.com>
2022-12-29 10:23:35 +0900

    Create 1.py

---
+++ c-study/1.py
@@ -0,0 +1,84 @@
+
+
+//1-1
+
+#include <stdio.h>
+int main()
+{
+    char input[501] = { 0 };
+    int alpha_count[26] = { 0 };
+    int i = 0;
+    int alpa = 0;
+
+    gets_s(input);
+
+    while (input[i] != 0)
+    {
+        if (input[i] > 96)
+        {
+            alpa = input[i] - 97;
+        }
+        else
+        {
+            alpa = input[i] - 65;
+        }
+        alpha_count[alpa] += 1;
+        i++;
+        alpa = 0;
+
+    }
+
+
+    for (int j = 0; j < 26; j++)
+    {
+
+        printf("%c : %d\n", j + 97, alpha_count[j]);
+    }
+
+    return 0;
+}
+
+//1-2
+
+
+#include <stdio.h>
+void count(char input[], int alpha_count[])
+{
+    int i = 0;
+    int alpa;
+
+    while (input[i] != NULL)
+    {
+        if (input[i] > 96)
+        {
+            alpa = input[i] - 97;
+        }
+        else
+        {
+            alpa = input[i] - 65;
+        }
+        alpha_count[alpa] += 1;
+        i++;
+        alpa = 0;
+
+    }
+}
+
+int main() {
+    char input[501] = { 0 };
+    int alpha_count[26] = { 0 };
+
+
+    gets_s(input);
+
+    count(input, alpha_count); //함수 호출
+
+    for (int j = 0; j < 26; j++)
+    {
+        printf("%c : %d\n", j + 97, alpha_count[j]);
+    }
+
+    return 0;
+}
+
+
