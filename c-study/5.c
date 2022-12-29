9fa1fadc3a49c31581158652ceb868ec17f9c9d1
yeoseojeong <121150215+yeoseojeong@users.noreply.github.com>
2022-12-30 00:07:47 +0900

    5.c

--- c-study/5.c
+++ c-study/5.c
@@ -4,29 +4,28 @@ int main(void)
     int i = 0;
     int j = 0;
     unsigned char c[3][10] = { 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29, 31,33,35,37,39,41,43,47,49,51,53,55,57,59,100 };
-    unsigned char *p = &c[0][0];
+    unsigned char* p = &c[0][0];
 
-   
 
-    unsigned char max = c[0][0];
-    unsigned char min = c[0][0];
 
-   
-    
-    for (i = 0; i < 3; i++)
+    int max = 0;
+    int min = 10000;
+
+
+
+    for (i = 0; i < 30; i++)
     {
-        for (j = 0; j < 10; j++)
-        {
-
+       
             if (*p > max)
             {
                 max = *p;
             }
-            else 
+            else
             {
                 max;
             }
-           
+          
+
             if (*p < min)
             {
                 min = *p;
@@ -35,13 +34,13 @@ int main(void)
             {
                 min;
             }
+        
             *p++;
-        }
-       
+
     }
-   
-  
+
+
 
     printf("%d", max - min);
-    
+
 }
