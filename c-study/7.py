7ec828110bc37621b61a4f768bc7a905c2f89b43
yeoseojeong <121150215+yeoseojeong@users.noreply.github.com>
2022-12-29 10:26:05 +0900

    Create 7.py

---
+++ c-study/7.py
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(void)
+{
+	char str[101] = { 0 };
+	int i;
+
+	
+	gets_s(str);
+
+	for (i = 0; str[i] != NULL; i++) 
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32; 
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z') 
+		{
+			str[i] = str[i] + 32;
+		}
+		
+	}
+	printf("%s", str);
+	return 0;
+	
+}
