d69425feb6305893d820d7d6f419af35a19882bc
yeoseojeong <121150215+yeoseojeong@users.noreply.github.com>
2022-12-29 10:31:32 +0900

    Create 8-1.py

---
+++ c-study/8-1.py
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(void)
+{
+    char str[101] = { 0 };
+    char arr[202] = { 0 };
+
+    int i;
+    int cnt = 0;
+
+    gets_s(str,101);
+
+    for (i = 0; str[i] != NULL; i++)
+    {
+        
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            arr[cnt] = str[i];
+            cnt++;
+        }
+        else if (str[i] >= 'A' && str[i] <= 'Z')
+        {                     
+            arr[cnt] = str[i] + 32;   
+            arr[cnt + 1] = str[i] + 32;
+            cnt= cnt+2;
+        }
+    }
+    printf("%s", arr);
+
+}
